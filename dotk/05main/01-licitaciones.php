1bcba805bf16c6c8b37d0ba98335974a7b5bd6b8
dlunna <dlunna@gmail.com>
2021-03-23 14:28:53 -0600

    Primera versión funcional

---
+++ dotk/05main/01-licitaciones.php
@@ -0,0 +1,52 @@
+<!-- Section -->
+<section id="licitaciones">
+  <div class="container">
+
+    <h1>Licitaciones</h1>
+    <hr />
+
+    <div class="row">
+
+      <!--Items licitaciones-->
+      <div class="col-3">
+          <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+          <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-lici1" role="tab" aria-controls="v-pills-home" aria-selected="true">No. LPN-ESTATAL-01-2017</a>
+          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici2" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-02-2017</a>
+          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici3" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-03-2017</a>
+          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici4" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-01-2018</a>
+          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici5" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-02-2018</a>
+          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici6" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-03-2018</a>
+          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici7" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-04-2018</a>
+          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici8" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-01-2019</a>
+          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici9" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-02-2019</a>
+          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici10" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-03-2019</a>
+          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici11" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-01-2020</a>
+          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici12" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-02-2020</a>
+          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici13" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-03-2020</a>
+        </div>
+      </div>
+
+      <!--Contenido licitaciones-->
+      <div class="col-9">
+        <div class="tab-content" id="v-pills-tabContent">
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2017.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2017.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-03-2017.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2018.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2018.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-03-2018.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-04-2018.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2019.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2019.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-03-2019.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2020.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2020.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-03-2020.html'); ?>
+        </div>
+      </div>
+
+    </div> <!--row-->
+
+  </div>
+</section>
+<!-- End Section-->
---
+++ dotk/05main/02-invitaciones.php
@@ -0,0 +1,70 @@
+<!-- Section -->
+<section id="invitaciones">
+  <div class="container">
+
+    <h1>Invitación a 3</h1>
+    <hr />
+
+    <div class="row">
+
+        <!--Lista de invitaciones-->
+        <div class="col-3">
+            <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-inv1" role="tab" aria-controls="v-pills-home" aria-selected="true">IA-913059981-E2-2017</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv2" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-913059981-E3-2017</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv3" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-913059981-E4-2017</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv4" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-913059981-E5-2017</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv5" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-913059981-E6-2017</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv6" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-913059981-E1-2018</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv7" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-913059981-E2-2018</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv8" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-913059981-E3-2018</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv9" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-913059981-E4-2018</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv10" role="tab" aria-controls="v-pills-profile" aria-selected="false">Bases y convocatoria cafeteria</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv11" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-ESTATAL-01-2018</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv12" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-ESTATAL-02-2018</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv13" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-913059981-E1-2019</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv14" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-913059981-E2-2019</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv15" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-913059981-E3-2019</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv16" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-913059981-E4-2019</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv17" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-ESTATAL-04-2019</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv18" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-913059981-E5-2019</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv19" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-913059981-E1-2020</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv20" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-913059981-E2-2020</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv21" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-913059981-E3-2020</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv22" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-913059981-E4-2020</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-inv23" role="tab" aria-controls="v-pills-profile" aria-selected="false">IA-ESTATAL-04-2020</a>
+            </div>
+        </div>
+
+        <div class="col-9">
+            <div class="tab-content" id="v-pills-tabContent">
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E2-2017.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E3-2017.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E4-2017.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E5-2017.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E6-2017.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E1-2018.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E2-2018.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E3-2018.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E4-2018.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/B-Y-C-CAFETERIA.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-ESTATAL-01-2018.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-ESTATAL-02-2018.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E1-2019.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E2-2019.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E3-2019.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E4-2019.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-ESTATAL-04-2019.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E5-2019.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E1-2020.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E2-2020.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E3-2020.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-913059981-E4-2020.html'); ?>
+                <?php include('./dotk/05main/02-Invitaciones/IA-ESTATAL-04-2020.html'); ?>
+            </div>
+        </div>
+    </div>
+
+  </div>
+</section>
+<!-- End Section-->

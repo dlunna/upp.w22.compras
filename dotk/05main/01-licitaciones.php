f1c70729fcf649643eb87add3418b2baa694c027
d4v <dlunna@gmail.com>
2023-06-20 19:13:17 -0600

    lpn 5 y 6 - 2023

--- dotk/05main/01-licitaciones.php
+++ dotk/05main/01-licitaciones.php
@@ -32,6 +32,8 @@
           <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici20" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-02-2023</a>
           <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici21" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-03-2023</a>
           <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici22" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-04-2023</a>
+          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici23" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-05-2023</a>
+          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici24" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-06-2023</a>
         </div>
       </div>
 
@@ -60,6 +62,8 @@
           <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2023.html'); ?>
           <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-03-2023.html'); ?>
           <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-04-2023.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-05-2023.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-06-2023.html'); ?>
         </div>
       </div>
 

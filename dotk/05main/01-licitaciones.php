5ef5d442fdee25872512ab4f0a605dba4d8bfd5f
d4v <dlunna@gmail.com>
2023-09-11 15:58:10 -0600

    lpn 7 2023

--- dotk/05main/01-licitaciones.php
+++ dotk/05main/01-licitaciones.php
@@ -10,60 +10,76 @@
       <!--Items licitaciones-->
       <div class="col-3">
           <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-          <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-lici1" role="tab" aria-controls="v-pills-home" aria-selected="true">No. LPN-ESTATAL-01-2017</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici2" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-02-2017</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici3" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-03-2017</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici4" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-01-2018</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici5" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-02-2018</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici6" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-03-2018</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici7" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-04-2018</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici8" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-01-2019</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici9" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-02-2019</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici10" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-03-2019</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici11" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-01-2020</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici12" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-02-2020</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici13" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-03-2020</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici14" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-01-2021</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici15" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-01-2022</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici16" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-02-2022</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici17" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-03-2022</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici18" role="tab" aria-controls="v-pills-profile" aria-selected="false">EA-913003989-N001-2022</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici19" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-01-2023</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici20" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-02-2023</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici21" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-03-2023</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici22" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-04-2023</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici23" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-05-2023</a>
-          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici24" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-06-2023</a>
+            <a class="nav-link active" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici25" role="tab" aria-controls="v-pills-profile" aria-selected="true">No. LPN-ESTATAL-07-2023</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici24" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-06-2023</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici23" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-05-2023</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici22" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-04-2023</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici21" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-03-2023</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici20" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-02-2023</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici19" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-01-2023</a>
+
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici18" role="tab" aria-controls="v-pills-profile" aria-selected="false">EA -913003989-N001-2022</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici17" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-03-2022</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici16" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-02-2022</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici15" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-01-2022</a>
+
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici14" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-01-2021</a>
+
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici13" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-03-2020</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici12" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-02-2020</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici11" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-01-2020</a>
+
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici10" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-03-2019</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici09" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-02-2019</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici08" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-01-2019</a>
+
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici07" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-04-2018</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici06" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-03-2018</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici05" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-02-2018</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici04" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-01-2018</a>
+            
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici03" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-03-2017</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici02" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-02-2017</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici01" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-01-2017</a>
+                   
         </div>
       </div>
 
       <!--Contenido licitaciones-->
       <div class="col-9">
         <div class="tab-content" id="v-pills-tabContent">
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2017.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2017.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-03-2017.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2018.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2018.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-03-2018.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-04-2018.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2019.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2019.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-03-2019.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2020.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2020.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-03-2020.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2021.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2022.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2022.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-03-2022.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/EA-913003989-N001-2022.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2023.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2023.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-03-2023.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-04-2023.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-07-2023.html'); ?>          
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-06-2023.html'); ?>          
           <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-05-2023.html'); ?>
-          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-06-2023.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-04-2023.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-03-2023.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2023.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2023.html'); ?>
+
+          <?php include('./dotk/05main/01-Licitaciones/EA-913003989-N001-2022.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-03-2022.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2022.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2022.html'); ?>
+
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2021.html'); ?>
+
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-03-2020.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2020.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2020.html'); ?>
+
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-03-2019.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2019.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2019.html'); ?>
+
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-04-2018.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-03-2018.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2018.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2018.html'); ?>
+
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-03-2017.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2017.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2017.html'); ?>
+         
         </div>
       </div>
 

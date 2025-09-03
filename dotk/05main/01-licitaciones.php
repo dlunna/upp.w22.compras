6b061468aec439403d0999171c66bae2fba88fae
d4v <dlunna@gmail.com>
2025-09-03 12:42:28 -0600

    lpn 412 y 424

--- dotk/05main/01-licitaciones.php
+++ dotk/05main/01-licitaciones.php
@@ -10,7 +10,10 @@
       <!--Items licitaciones-->
       <div class="col-3">
           <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-            <a class="nav-link active" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici36" role="tab" aria-controls="v-pills-profile" aria-selected="true"> No. LPN-ESTATAL-02-2025</a>
+            <a class="nav-link active" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici38" role="tab" aria-controls="v-pills-profile" aria-selected="true"> No. LPN-EA-913003989-N424-2025</a>
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici37" role="tab" aria-controls="v-pills-profile" aria-selected="false"> No. LPN-EA-913003989-N412-2025</a>
+
+            <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici36" role="tab" aria-controls="v-pills-profile" aria-selected="false"> No. LPN-ESTATAL-02-2025</a>
             <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici35" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-01-2025</a>
             
             <a class="nav-link"        id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-lici34" role="tab" aria-controls="v-pills-profile" aria-selected="false">No. LPN-ESTATAL-07-2024</a>
@@ -62,6 +65,9 @@
       <!--Contenido licitaciones-->
       <div class="col-9">
         <div class="tab-content" id="v-pills-tabContent">
+          <?php include('./dotk/05main/01-Licitaciones/LPN-EA-913003989-N424-2025.html'); ?>
+          <?php include('./dotk/05main/01-Licitaciones/LPN-EA-913003989-N412-2025.html'); ?>
+
           <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-02-2025.html'); ?>
           <?php include('./dotk/05main/01-Licitaciones/LPN-ESTATAL-01-2025.html'); ?>
 
